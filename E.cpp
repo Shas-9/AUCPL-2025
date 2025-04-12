3d2c0e341fe8181ddfafbf2c1b4702d28174c595
Shas9 <xxshas9xx@gmail.com>
2025-04-12 10:26:50 +0930

    Created branch Round1

---
+++ E.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+    // cin >> tc; //comment out if 1 case
+    while(tc--) {
+        solve();
+    }
+
+    return 0;
+}
