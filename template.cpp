42f185f7bc3466c3fde31f13bb70c0509cf12a23
Shas9 <xxshas9xx@gmail.com>
2025-10-26 15:27:42 +1030

    round6

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a, i < (b); ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 typedef long long ll;
@@ -16,11 +16,6 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int tc = 1;
-    // cin >> tc; //comment out if 1 case
-    while(tc--) {
-        solve();
-    }
 
     return 0;
 }
