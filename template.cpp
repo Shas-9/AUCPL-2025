69c041e86aa9eb8bd181c7f615c25186519354a4
Shas9 <xxshas9xx@gmail.com>
2025-10-26 15:28:43 +1030

    modified template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a, i < (b); ++i)
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 typedef long long ll;
