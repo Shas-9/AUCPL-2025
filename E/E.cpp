a533048ca650b4026dc5c8b90a4a5171dd33d1b2
Sharzil Hasnine <a1881771@ce-enm108-10.ad.adelaide.edu.au>
2026-03-14 12:45:13 +1030

    j

--- E/E.cpp
+++ E/E.cpp
@@ -20,34 +20,16 @@ int main() {
     cin >> n;
 
     vector<int> v(n);
-    int p = 0;
-    int ops = 0;
+    int p;
 
-    while (ops < 125) {
-        cin >> v[0];
-        for (int i = 1; i < n; i++) cin >> v[i];
+    for (int i = 0; i < 125; i++) {
+        cout << 1 << " " << n << endl;
+        cout.flush();
+
+        for (int j = 0; j < n; j++) cin >> v[j];
         cin >> p;
 
         if (p >= 70) break;
-
-        int l = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (v[i] == 0) {
-                l = i;
-                break;
-            }
-        }
-
-        if (l == -1) break;
-
-        int r = l;
-        while (r + 1 < n && v[r + 1] == 0) r++;
-
-        cout << l + 1 << " " << r + 1 << "\n";
-        cout.flush();
-
-        ops++;
     }
 
     int tc = 1;
