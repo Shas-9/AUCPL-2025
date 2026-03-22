3247d63c3444d65a0636a1e1eadb389a04a99fae
Sharzil Hasnine <a1881771@ce-iwb23-67.ad.adelaide.edu.au>
2026-03-22 10:42:15 +1030

    round 1

---
+++ E/E.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+    // cin >> tc; //comment out if 1 case
+    while(tc--) {
+        solve();
+    }
+
+    return 0;
+}
