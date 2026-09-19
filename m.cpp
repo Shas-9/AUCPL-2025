fdcc30995ac1e6701eb9f487a10dfa7b7e7b0326
Sharzil Hasnine <a1881771@ce-enm108-73.ad.adelaide.edu.au>
2026-09-19 14:56:22 +0930

    m

--- m.cpp
+++ m.cpp
@@ -1,9 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define rep(i, a, b) for(int i = a; i < (b); ++i) 
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void solve() {
+    
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
 
     int n, k, m;
     cin >> n >> k >> m;
