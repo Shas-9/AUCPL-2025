2d08b7ac8b43d6b714c86c90b6f4bb8e2203005b
Sharzil <132335949+Shas-9@users.noreply.github.com>
2026-09-19 13:54:34 +0930

    Create m.cpp

---
+++ m.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, k, m;
+    cin >> n >> k >> m;
+
+    vector<vector<int>> children(n + 1);
+    vector<int> parent(n + 1, 0);
+
+    // Input gives parent of rooms 2..n
+    for (int v = 2; v <= n; v++) {
+        cin >> parent[v];
+        children[parent[v]].push_back(v);
+    }
+
+    /*
+        cnt[v][d] = number of nodes exactly d edges below v
+    */
+    vector<vector<int>> cnt(n + 1, vector<int>(m + 1, 0));
+
+    for (int v = 1; v <= n; v++) {
+        cnt[v][0] = 1;
+    }
+
+    /*
+        Since every parent has a smaller construction dependency than
+        its children in the given rooted tree, we explicitly create a
+        traversal order and process it backwards.
+    */
+    vector<int> order;
+    order.reserve(n);
+
+    stack<int> st;
+    st.push(1);
+
+    while (!st.empty()) {
+        int v = st.top();
+        st.pop();
+
+        order.push_back(v);
+
+        for (int u : children[v]) {
+            st.push(u);
+        }
+    }
+
+    // Bottom-up DP
+    reverse(order.begin(), order.end());
+
+    for (int v : order) {
+        for (int u : children[v]) {
+            for (int d = 1; d <= m; d++) {
+                cnt[v][d] += cnt[u][d - 1];
+            }
+        }
+    }
+
+    /*
+        Recover path from 1 to k.
+    */
+    vector<int> path;
+
+    int cur = k;
+    while (cur != 0) {
+        path.push_back(cur);
+        cur = parent[cur];
+    }
+
+    reverse(path.begin(), path.end());
+
+    /*
+        dp[i] = minimum cost to reach path[i]
+    */
+    int L = (int)path.size();
+
+    const long long INF = (1LL << 60);
+    vector<long long> dp(L, INF);
+
+    dp[0] = 0;
+
+    for (int i = 1; i < L; i++) {
+        for (int d = 1; d <= m && d <= i; d++) {
+            int from = path[i - d];
+
+            dp[i] = min(
+                dp[i],
+                dp[i - d] + cnt[from][d]
+            );
+        }
+    }
+
+    cout << dp[L - 1] << '\n';
+
+    return 0;
+}
